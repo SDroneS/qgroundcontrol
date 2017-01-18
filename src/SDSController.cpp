214511639a4602b5b8c537716cecfa4848c49508
Rami&Filip <mikor.projekt@gmail.com>
2017-01-18 10:34:16 +0100

    SDS Flag thread - working

--- src/SDSController.cpp
+++ src/SDSController.cpp
@@ -5,7 +5,7 @@ void MyWorker::run(void)
     // Setting Flag File path
     _myPathToFlagFile = "/home/kasper/Repos/SDS_Flag.cfg";
     QFile _myFile(_myPathToFlagFile);
-    if(!_myFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    if(!_myFile.open(QIODevice::ReadWrite | QIODevice::Text))
     {
        emit showMyMsg(tr("Cannot open the SDS flag file!"));
        emit workerResult(false);
@@ -19,14 +19,13 @@ void MyWorker::run(void)
     // Default value for flag file which is set after flag reading
     _data_out = "0";
 
-    QTextStream in(&_myFile);
-    QTextStream out(&_myFile);
+    QTextStream myStream(&_myFile);
 
     // Reading SDS flag file thread loop
     while (true)       //(!in.atEnd())
     {
        // While Flag value is set to 1
-       if(in.readLine(1) == "1")
+       if(myStream.readLine(1) == "1")
        {
            // While WPTs file doesn't exist
            if(!_myWPTsFile.exists())
@@ -38,7 +37,8 @@ void MyWorker::run(void)
 
            // Setting SDS flag file value to default
            _myFile.resize(0);
-           out << _data_out;
+           QTextStream myStream(&_myFile);
+           myStream << _data_out;
 
            // Loading the SDS WPTs file to autopilot
            emit loadWPTs(_myPathToWPTsFile);
